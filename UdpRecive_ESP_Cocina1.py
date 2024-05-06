db2af154aafa815db6bc3f190706a60519cbb946
Daniel Villafane <villafapd@outlook.com>
2024-05-06 20:34:49 -0300

    Upgrade Concina1

---
+++ UdpRecive_ESP_Cocina1.py
@@ -0,0 +1,11 @@
+import socket
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 15103 #Debe ser el mismo que se configuró en el ESP_Bano1
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(4096)
+    print("received message:", data)
